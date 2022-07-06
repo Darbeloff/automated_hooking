3b8eb7e7bbc85fa3598271ec227ec3414d5dc653
Quinn Bowers <q.bowers@outlook.com>
2022-07-06 12:12:40 -0400

    minor arduino bridge fixes

--- src/hook_bridge.py
+++ src/hook_bridge.py
@@ -6,7 +6,7 @@ import time
 
 class HookBridge:
     """
-    This class interfaces with the hook-mounted arduino over serial, recieving updates about the orientation and acceleration of the board. This class can also command the nano to open and close the retainer
+    This class interfaces with the hook-mounted arduino over serial, receiving updates about the orientation and acceleration of the board. This class can also command the nano to open and close the retainer
     """
 
     # ARDUINO_PORT = 'ttyUSB4'
@@ -14,49 +14,50 @@ class HookBridge:
     BAUDRATE = 115200
 
     state = {
-        '\{acceleration\}': [],
-        '\{gryo\}': [],
-        '\{orientation\}': []
+        'acceleration': [],
+        'gyro': [],
+        'orientation': []
     }
 
     def __init__(self, debug=False):
         self.debug = debug
         
-        self.port = serial.Serial(ARDUINO_PORT, BAUDRATE)
+        self.port = serial.Serial(self.ARDUINO_PORT, self.BAUDRATE)
 
+        self.set_retainer(True)
         while True:
             self.receive_serial()
-            time.sleep(100)
+            time.sleep(0.005)
+
+            print(self.state)
     
     def set_retainer(self, open):
         """ Write to the serial port with characters indicating whether to open or close
         """
-        print("command:" + open)
+        # print("command:" + str(open))
         if open:
             self.port.write(b'o')
         else:
             self.port.write(b'c')
 
-    def receive_serial(self, open):
-        in_line = self.port.readline()
-        
-        print(in_line)
+    def receive_serial(self):
+        in_line = self.port.readline().decode('utf-8')
 
-        if in_line == None: return
+        if in_line == None:
+            print("None")
+            return
 
         for header in self.state.keys():
-            print(header)
-            if in_line.contains(header):
+            if header in in_line:
                 in_line = in_line.replace(header, '') # remove header string
-                self.state[header] = in_line[1,-1].split(',') # clean brackets and split on comma 
+                self.state[header] = in_line[3:-3].split(',') # clean brackets and split on comma 
                 return
 
-        
+    
 
-
-        
-
-    def on_close(self):
+    def __del__(self):
+        """ Destructor ensures serial port is properly closed
+        """
         self.port.close()
 
 
