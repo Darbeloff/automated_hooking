d3912ef86fc8d5c5fd3a4c60e235b487ee2a8b7c
darbeloff <darbeloff@mit.edu>
2022-07-25 17:39:42 -0400

    Added logfile, fixed Coord bugs

--- src/Utils.py
+++ src/Utils.py
@@ -7,6 +7,36 @@ from geometry_msgs.msg import Transform
 
 
 
+
+class LogFile:
+    def __init__(self, filename, headers):
+        #headers should be an array of headers
+        #filename should just be a name, string
+        self.filename = filename
+        self.headers = headers
+        with open(self.filename, "w") as file:
+            file.write("time")
+            for header in headers:
+                file.write(",")
+                file.write(header)
+
+            file.write('\n')
+    #call this every time you want to log, give it the ros timestamp
+    def log(self, time, columns):
+        try:
+            assert(len(columns) == len(self.headers))
+        except AssertionError:
+            rospy.logwarn("Attempting to log an inconsistant number of values")
+            return
+
+        with open(self.filename, "a") as file:
+            file.write(str(time))
+            for col in columns:
+                file.write(",")
+                file.write(str(col))
+
+            file.write('\n')
+
 class Vector:
     """
     A handy class to convert between iterables and .xyzw datatypes
@@ -36,25 +66,35 @@ class Coord:
     A handy class to do coordinate arithmetic with. Usefully converts poses, transforms, and matrices
     """
 
-    I = Coord(np.eye(4)) # the identity coordinate
-
+    # I = Coord(np.eye(4)) # the identity coordinate
     def __init__(self, *inputs):
         if len(inputs) == 2:
             # input is two vectors
-            P = tf_t.translation_matrix( inputs[0] )
-            if len(inputs[1]) == 4:
-                M = tf_t.quaternion_matrix( inputs[1] )
+            position = inputs[0]
+            orientation = inputs[1]
+
+            # cleanse position input
+            if len( position ) != 3:
+                position = list(position)
+                position.append(0)
+
+            P = tf_t.translation_matrix( position )
+
+            # accept orientation as euler angles or quaternion
+            if len(orientation) == 4:
+                M = tf_t.quaternion_matrix( orientation )
             else:
-                M = tf_t.euler_matrix( *inputs[1], 'rxyz' )
+                M = tf_t.euler_matrix( orientation[0], orientation[1], orientation[2], 'rxyz' )
             
             self.T = P.dot(M)
+            
         elif isinstance(inputs[0], np.ndarray):
             # input is a matrix
             # TODO: input is lone position vector
-            self.T = tf
+            self.T = inputs[0]
         else:
             # input is a message
-            x,q = search_recursive(tf, [['translation','position'],['rotation','orientation']])
+            x,q = search_recursive(inputs[0], [['translation','position'],['rotation','orientation']])
 
             P = tf_t.translation_matrix(Vector.to_array( x ))
             M = tf_t.quaternion_matrix( Vector.to_array( q ))
@@ -84,7 +124,7 @@ class Coord:
         return x
 
     def __neg__(self):
-        return Coord( numpy.linalg.inv(self.T) )
+        return Coord( np.linalg.inv(self.T) )
 
     def __add__(self, other):
         return Coord(self.T.dot(other.T))
@@ -106,7 +146,7 @@ class Coord:
     def __idiv__(self,other):
         self -= other
 
-    def __abs__(self,other):
+    def __abs__(self):
         return np.linalg.norm(self.get_translation())
 
     def __repr__(self):
